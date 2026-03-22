d06c45850b0b749b0cdf0678ba051a7ab3c4f348
txjon <jon@housepartydistro.com>
2026-03-21 17:44:27 -0700

    Costing KPI bar - 10% margin, stacked layout right justified

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("25c3ba0f5b1398cf")
+/******/ 		__webpack_require__.h = () => ("612d3e926ce9c545")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "10b58cd987f4464e"; }
+/******/ 		__webpack_require__.h = function() { return "e87f0e788faea297"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
